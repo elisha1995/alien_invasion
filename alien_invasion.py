c04e589ffa646f3dc7834f3f0e232d2964517718
elisha1995 <elishasamuel01@gmail.com>
2023-04-16 12:17:58 +0000

    Controlling the Frame Rate

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,8 +8,9 @@ class AlienInvasion:
 
     def __int__(self):
         """Initialize the game, and create game resources."""
-
         pygame.init()
+
+        self.clock = pygame.time.Clock()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
@@ -28,6 +29,7 @@ class AlienInvasion:
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
